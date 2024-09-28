1233e1166e2963ab96026ea2a4eb61ab646f2268
raulDeDevWorld <proyection009@gmail.com>
2024-09-27 23:54:06 -0400

    DC

--- src/app/(layout)/MisCambios/page.jsx
+++ src/app/(layout)/MisCambios/page.jsx
@@ -18,9 +18,13 @@ export default function Home() {
   const [state, setState] = useState({})
   const [estado, setEstado] = useState('')
   const refFirst = useRef(null);
-
+  const [profileIMG, setProfileIMG] = useState('')
+  const [row, setRow] = useState(-1)
   function onChangeFilter(e) {
     setFilter(e.target.value)
+  }
+  function handlerProfileIMG(img) {
+    setProfileIMG(img)
   }
   function sortArray(x, y) {
     if (x['usuario'].toLowerCase() < y['usuario'].toLowerCase()) { return -1 }
@@ -71,104 +75,206 @@ export default function Home() {
             })}
           </div>
         </div>
-        <br />
-        <br />
-        <table className="w-full min-w-[2700px] border-[1px] bg-white text-[14px] text-left text-gray-500 border-t-4 border-t-gray-400">
-          <thead className="text-[14px] text-gray-700 uppercase bg-white">
+        <table className="w-full min-w-[4300px] border-[1px] bg-white text-[14px] text-left text-gray-500 border-t-4 border-t-gray-400">
+          <thead className="text-[14px] text-gray-700 uppercase bg-gray-800 text-white ">
+
+            <tr>
+              <th scope="col" className="w-[50px] px-3 py-1">
+
+              </th>
+              <th scope="col" className="w-[50px] px-3 py-1">
+
+              </th>
+              <th scope="col" colSpan={9} className=" text-center bg-red-500 px-3 py-1" >
+                DATOS DE EMISIÓN
+              </th>
+              <th scope="col" colSpan={5} className=" text-center bg-green-500  px-3 py-1">
+                DATOS PARA RECEPCIÓN
+              </th>
+              <th scope="col" colSpan={6} className=" text-center bg-yellow-500 px-3 py-1">
+                DATOS DE TRANSACCION
+              </th>
+              <th scope="col" colSpan={4} className=" text-center bg-blue-500 px-3 py-1">
+                Cuenta receptora Bottak
+              </th>
+              <th scope="col" className="w-[50px] px-3 py-1">
+
+              </th>
+            </tr>
+
             <tr>
               <th scope="col" className="w-[50px] px-3 py-3">
                 #
               </th>
-              <th scope="col" className=" px-3 py-3 text-center">
+              <th scope="col" className=" px-3 py-3">
                 estado
               </th>
               <th scope="col" className=" px-3 py-3">
-                Usuario
+                Nombre
               </th>
               <th scope="col" className=" px-3 py-3">
-                DNI de usuario
+                DNI
               </th>
               <th scope="col" className=" px-3 py-3">
-                Pais de usuario
+                Pais
               </th>
               <th scope="col" className=" px-3 py-3">
-                Whatsapp de usuario
-              </th>
-              <th scope="col" className=" px-3 py-3">
-                Cuenta de usuario
+                Celular
               </th>
               <th scope="col" className=" px-3 py-3">
                 Nombre de banco
               </th>
               <th scope="col" className=" px-3 py-3">
-                Divisa de envio
+                Cuenta bancaria
               </th>
               <th scope="col" className=" px-3 py-3">
-                Importe
+                Direccion de wallet
               </th>
               <th scope="col" className=" px-3 py-3">
-                Divisa de cambio
+                Red
               </th>
               <th scope="col" className=" px-3 py-3">
-                Importe con el <br /> cambio aplicado
+                Divisa emisión
+              </th>
+              {/* Destinatario */}
+              <th scope="col" className=" px-3 py-3">
+                Nombre de banco
               </th>
               <th scope="col" className=" px-3 py-3">
-                ID de transaccion
+                Cuenta bancaria
+              </th>
+              <th scope="col" className=" px-3 py-3">
+                Direccion de wallet
+              </th>
+              <th scope="col" className=" px-3 py-3">
+                Red
+              </th>
+              <th scope="col" className=" px-3 py-3">
+                Divisa recepción
+              </th>
+              {/* Transaccion */}
+              <th scope="col" className=" px-3 py-3">
+                Importe mas comision
+              </th>
+              <th scope="col" className=" px-3 py-3">
+                Comision
+              </th>
+              <th scope="col" className=" px-3 py-3">
+                Importe cambio
               </th>
               <th scope="col" className=" px-3 py-3">
                 Fecha
               </th>
+              <th scope="col" className=" px-3 py-3">
+                ID de transacción
+              </th>
+              <th scope="col" className=" px-3 py-3">
+                Baucher
+              </th>
+              <th scope="col" className=" px-3 py-3">
+                Nombre de banco
+              </th>
+              <th scope="col" className=" px-3 py-3">
+                Cuenta bancaria
+              </th>
+              <th scope="col" className=" px-3 py-3">
+                Billetera Bottak
+              </th>
+              <th scope="col" className=" px-3 py-3">
+                Red Bottak
+              </th>
+              <th scope="col" className=" px-3 py-3">
+                Actualizar
+              </th>
             </tr>
+
+
           </thead>
           <tbody>
             {cambiosDB && cambiosDB !== undefined && Object.values(cambiosDB).sort((a, b) => a.date - b.date).map((i, index) => {
               return ((i.dni !== undefined && i.dni.toLowerCase().includes(filter.toLowerCase())) ||
                 (i.usuario !== undefined && i.usuario.toLowerCase().includes(filter.toLowerCase()))) &&
-                <tr className={`text-[14px] border-b hover:bg-gray-100  ${index % 2 === 0 ? '' : ''} `} key={index}>
-                  <td className="px-3 py-4  flex  ">
-                    <span className='h-full flex py-2'>{index + 1}</span>
-                  </td>
-                  {/* {console.log(i['estado'])} */}
-                  <td className="min-w-32 px-3 py-4  ">
-                    <span className={`w-full block py-5 px-10 rounded-[10px] ${i.estado == 'En verificación' && 'bg-gray-100'}   ${i.estado == 'Transfiriendo' && 'bg-yellow-300'}   ${i.estado == 'Exitoso' && 'bg-green-400'} ${i.estado == 'Rechazado' && 'bg-red-400'}`}>{i['estado']}</span>
-                  </td>
-                  <td className="min-w-32 px-3 py-4  ">
-                    {i['usuario']}
-                  </td>
-                  <td className="min-w-32 px-3 py-4  ">
-                    {i['dni']}
-                  </td>
-                  <td className="min-w-32 px-3 py-4  ">
-                    {i['pais']}
-                  </td>
-                  <td className="min-w-32 px-3 py-4  ">
-                    {i['whatsapp']}
-                  </td>
-                  <td className="min-w-32 p-3">
-                    {i['cuenta bancaria']}
-                  </td>
-                  <td className="min-w-32 p-3">
-                    {i['banco']}
-                  </td>
-                  <td className=" p-3">
-                    {i['divisa de usuario']}
-                  </td>
-                  <td className="px-3 py-4  ">
-                    {i['importe']}
-                  </td>
-                  <td className=" p-3">
-                    {i['divisa de cambio']}
-                  </td>
-                  <td className="min-w-32 p-3">
-                    {i['cambio']}
-                  </td>
-                  <td className="min-w-32 p-3">
-                    {i['uuid']}
-                  </td>
-                  <td className="min-w-32 p-3">
-                    {i['fecha']}
-                  </td>
-                </tr>
+                <tr className={`text-[14px] border-b border-gray-50  py-1 transition-all ${index === row ? 'bg-gray-100' : 'bg-gray-200'} ${index % 2 === 0 ? '' : ''} `} key={index} onClick={() => setRow(index)}>
+                <td className="px-3 py-0  flex  ">
+                  <span className='h-full flex py-0'>{index + 1}</span>
+                </td>
+                {/* {console.log(i['estado'])} */}
+                <td className="min-w-32 px-3 py-0  ">
+                 <span className={`w-full block py-5 px-10 rounded-[10px] ${i.estado == 'En verificación' && 'bg-gray-100'}   ${i.estado == 'Transfiriendo' && 'bg-yellow-300'}   ${i.estado == 'Exitoso' && 'bg-green-400'} ${i.estado == 'Rechazado' && 'bg-red-400'}`}>{i['estado']}</span>
+                </td>
+                <td className="min-w-32 px-3 py-0  ">
+                  {i['usuario']}
+                </td>
+                <td className="min-w-32 px-3 py-0  ">
+                  {i['dni']}
+                </td>
+                <td className="min-w-32 px-3 py-0  ">
+                  {i['pais']}
+                </td>
+                <td className="min-w-32 px-3 py-0  ">
+                  {i['whatsapp']}
+                </td>
+                <td className="min-w-32 px-2">
+                  {i['banco remitente']}
+                </td>
+                <td className="min-w-32 px-2">
+                  {i['cuenta bancaria']}
+                </td>
+                <td className="min-w-32 px-2">
+                  {i['billetera remitente']}
+                </td>
+                <td className="min-w-32 px-2">
+                  {i['red bottak']}
+                </td>
+                <td className=" px-2">
+                  {i['divisa de usuario']}
+                </td>
+                <td className="min-w-32 px-2">
+                  {i['nombre de banco']}
+                </td>
+                <td className="min-w-32 px-2">
+                  {i['cuenta destinatario']}
+                </td>
+                <td className="min-w-32 px-2">
+                  {i['billetera destinatario']}
+                </td>
+                <td className="min-w-32 px-2">
+                  {i['red destinatario']}
+                </td>
+                <td className="min-w-32 px-2">
+                  {i['red destinatario']}
+                </td>
+                <td className="px-3 py-0  ">
+                  {i['importe']}
+                </td>
+                <td className=" px-2">
+                  {i['comision']}
+                </td>
+                <td className="min-w-32 px-2">
+                  {i['cambio']}
+                </td>
+                <td className="min-w-32 px-2">
+                  {i['fecha']}
+                </td>
+                <td className="min-w-32 px-2">
+                  {i['uuid']}
+                </td>
+                <td className="min-w-32 px-2">
+                  <img src={i.url} className={`${i.url === profileIMG ? 'fixed right-0 left-0 top-0 bottom-0 m-auto portrait:w-[100vw] landscape:h-[100vh] z-50' : 'h-[150px] w-[150px] object-contain'}`} onClick={() => handlerProfileIMG(i.url)} alt="" />
+                </td>
+                <td className="min-w-32 px-2">
+                  {i['banco bottak']}
+                </td>
+                <td className="min-w-32 px-2">
+                  {i['cuenta bottak']}
+                </td>
+                <td className="min-w-32 px-2">
+                  {i['billetera bottak']}
+                </td>
+                <td className="min-w-32 px-2">
+                  {i['red bottak']}
+                </td>
+              </tr>
             })
             }
           </tbody>
@@ -178,3 +284,56 @@ export default function Home() {
   )
 }
 
+
+
+
+
+
+
+
+
+// <tr className={`text-[14px] border-b hover:bg-gray-100  ${index % 2 === 0 ? '' : ''} `} key={index}>
+// <td className="px-3 py-4  flex  ">
+//   <span className='h-full flex py-2'>{index + 1}</span>
+// </td>
+// {/* {console.log(i['estado'])} */}
+// <td className="min-w-32 px-3 py-4  ">
+//   <span className={`w-full block py-5 px-10 rounded-[10px] ${i.estado == 'En verificación' && 'bg-gray-100'}   ${i.estado == 'Transfiriendo' && 'bg-yellow-300'}   ${i.estado == 'Exitoso' && 'bg-green-400'} ${i.estado == 'Rechazado' && 'bg-red-400'}`}>{i['estado']}</span>
+// </td>
+// <td className="min-w-32 px-3 py-4  ">
+//   {i['usuario']}
+// </td>
+// <td className="min-w-32 px-3 py-4  ">
+//   {i['dni']}
+// </td>
+// <td className="min-w-32 px-3 py-4  ">
+//   {i['pais']}
+// </td>
+// <td className="min-w-32 px-3 py-4  ">
+//   {i['whatsapp']}
+// </td>
+// <td className="min-w-32 p-3">
+//   {i['cuenta bancaria']}
+// </td>
+// <td className="min-w-32 p-3">
+//   {i['banco']}
+// </td>
+// <td className=" p-3">
+//   {i['divisa de usuario']}
+// </td>
+// <td className="px-3 py-4  ">
+//   {i['importe']}
+// </td>
+// <td className=" p-3">
+//   {i['divisa de cambio']}
+// </td>
+// <td className="min-w-32 p-3">
+//   {i['cambio']}
+// </td>
+// <td className="min-w-32 p-3">
+//   {i['uuid']}
+// </td>
+// <td className="min-w-32 p-3">
+//   {i['fecha']}
+// </td>
+// </tr>
