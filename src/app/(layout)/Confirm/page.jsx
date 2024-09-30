e2bd7d51ebbff7e5c38ad17ad7e8d1a6f2e87cb7
raulDeDevWorld <proyection009@gmail.com>
2024-09-29 23:17:19 -0400

    DC

--- src/app/(layout)/Confirm/page.jsx
+++ src/app/(layout)/Confirm/page.jsx
@@ -136,7 +136,7 @@ console.log(destinatario)
                             </tr>
                             <tr className=" text-[14px]  border-b border-[#00000040] hover:bg-gray-50 " >
                                 <td className="px-2 py-2 bg-[#00000020]   font-bold  text-gray-900 ">
-                                    {destinatario['red'] ? 'Red' : 'Banco destinatario'}
+                                    {destinatario['red destinatario'] ? 'Red' : 'Banco destinatario'}
                                 </td>
                                 <td className="px-2 py-2  text-gray-900  border-r border-[#00000040]">
                                     {destinatario['nombre de banco'] && destinatario['nombre de banco']}
